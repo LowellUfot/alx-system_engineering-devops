47538e08e1624e874a11d95aa38f115392ee05e2
LowellUfot <lowellufot@gmail.com>
2023-04-09 15:29:41 -0700

    puppet script to configure nginx server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,61 +1,61 @@
-# define directory for custom page in a variable
+# Define directory for custom page in a variable
 $doc_root = "/var/www/html"
 
-# execute apt package update
+# Execute apt package update
 exec { 'apt-get update':
   command => '/usr/bin/apt-get update'
 }
 
-# install nginx
+# Install nginx
 package { 'nginx':
   ensure  => "installed",
   require => Exec['apt-get update']
 }
 
-# adjust the firewall
+# Adjust the firewall
 exec { 'firewall':
   command => 'ufw allow "Nginx HTTP"',
   require => Package['nginx']
 }
 
-# create directory for doc_root
+# Create directory for doc_root
 file { $doc_root:
   ensure => "directory"
 }
 
-# custom string
+# Custom string
 $str = "Hello World!"
 
-# content of index.html
-file { "$doc_root/index.html":
+# Content of index.html
+file {"$doc_root/index.html":
   content => $str,
   require => File[$doc_root]
 }
 
-# redirect link
+# Redirect link
 $rdr = "https://www.youtube.com/watch?v=QH2-TGUlwu4"
 
-# configure /redirect_me
+# Configure /redirect_me
 file { '/etc/nginx/sites-available/default':
-  ensure  => file,
+  ensure => file,
   content => "
-	server {
-	  listen 80;
-	  root $doc_root;
-	  index index.html;
+    server {
+      listen 80;
+      root $doc_root;
+      index index.html;
 
-
-	  location /redirect_me {
-		return 301 $rdr;
-	  }
-	}
-	",
+      location /redirect_me {
+        return 301 $rdr;
+      }
+    }
+  ",
   require => Package['nginx'],
   notify  => Service['nginx']
+}
 
-# create service to restart nginx
+# Create service to restart nginx
 service { 'nginx':
-  ensure  => running,
-  enable  => true,
+  ensure => running,
+  enable => true,
   require => File['/etc/nginx/sites-available/default']
 }
