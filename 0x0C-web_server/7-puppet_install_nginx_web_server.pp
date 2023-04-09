32d257f49b3793b92dac0bcfd4d29f61c6bf054f
LowellUfot <lowellufot@gmail.com>
2023-04-09 16:33:02 -0700

    puppet manifest

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,26 +1,32 @@
-# Install NginX
-# With puppet
+#Install Nginx with Puppet
 
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
+# Execute apt package update
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update'
 }
 
+# Install nginx
 package { 'nginx':
-  ensure  => installed,
-  require => Exec['apt-get-update'],
+  ensure  => "installed",
+  require => Exec['apt-get update']
 }
 
+# Content of index.html
 file { '/var/www/html/index.html':
-  content => 'Hello World!',
-  require => Package['nginx'],
+  content => "Hello World!",
+  require => Package['nginx']
 }
 
-exec {'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell'
+#Configure redirect in config file
+file_line { 'default':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4;'
 }
 
+# Create service to restart nginx
 service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+  ensure => running,
+  require => Package['nginx']
 }
